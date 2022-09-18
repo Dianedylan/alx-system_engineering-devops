74afed5b609aea6935ad34d4480e7da6fc1c331c
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-09-18 06:59:41 -0700

    1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# install puppet-lint -v 2.5.0
-
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+# Task 1: Install a package
+exec {
+  command =>  'puppet-lint -v 2.1.1',
 }

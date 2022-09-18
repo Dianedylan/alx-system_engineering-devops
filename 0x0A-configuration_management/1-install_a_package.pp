5f2704fcb296b726d181164f8e1c88b129668e8a
Dianedylan <dianagatwiriikiamba@gmail.com>
2022-09-18 16:47:20 +0300

    puppet config. mgt

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,4 @@
-# Install the puppet-lint package
-package { 'puppet-lint':
-  ensure   => '2.5.0',
-  provider => 'gem',
+# Task 1: Install a package
+exec {
+  command =>  'puppet-lint -v 2.1.1',
 }
